3c51a3666d9176fda43263e6c56400ca190cd27f
snehal9066 <147644050+snehal9066@users.noreply.github.com>
2024-01-14 22:37:23 +0530

    Add files via upload

---
+++ LAB CYCLE 1 HAAPY NUM IN RANGE.py
@@ -0,0 +1,6 @@
+def happynum(x1,x2):
+        for i in range(x1,x2):
+          happynumb(i)
+list1=int(input("Enter the lower limit here:"))
+list2=int(input("Enter the upper limit here:"))
+happynum(list1,list2)
